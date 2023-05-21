dad296a65fa3d17ff943e1ef5b5b581bdd951947
Betul Erol <90437799+fmbetul@users.noreply.github.com>
2023-05-21 16:26:37 -0400

    CropCluster-v2.0.R

--- R/CropCluster.R
+++ R/CropCluster.R
@@ -1,45 +1,42 @@
-global_vars <- ls(envir = globalenv())
-clean_vars <- global_vars[grepl("_Clean$", global_vars)]
-all_vars <- c("Leftovers", clean_vars)
-utils::globalVariables(all_vars)
-rm(global_vars)
-rm(clean_vars)
-rm(all_vars)
-
-#' Crop a cluster based on its UMAP coordinates
+#' cropCluster function
 #'
-#' This function crops a cluster based on its UMAP coordinates and saves the cleaned cluster as a new cluster with a specified name.
+#' This function selects a cluster from the Seurat object based on the provided cluster name or number,
+#' adjusts its boundaries based on provided min and max coordinates in both axes, and then stores
+#' the cleaned cluster and leftovers in datacleaning_list in the global environment.
 #'
-#' @param SeuData A Seurat object containing the data to be cropped.
-#' @param ClusterName A character string specifying the name of the cluster to be cropped.
-#' @param ClusterNo An integer specifying the number of the cluster to be cropped. If specified, the function will ignore the ClusterName parameter and use the ClusterNo instead. Default is 0.
-#' @param min.x A numeric value specifying the minimum x-coordinate of the cropped cluster on the UMAP plot. Default is -100.
-#' @param max.x A numeric value specifying the maximum x-coordinate of the cropped cluster on the UMAP plot. Default is 100.
-#' @param min.y A numeric value specifying the minimum y-coordinate of the cropped cluster on the UMAP plot. Default is -100.
-#' @param max.y A numeric value specifying the maximum y-coordinate of the cropped cluster on the UMAP plot. Default is 100.
+#' @param SeuData A Seurat object that contains the data of the clusters.
+#' @param ClusterName Name of the cluster to be selected.
+#' @param ClusterNo Number of the cluster to be selected. The default is `0`.
+#' @param min.x Minimum x coordinate of the new cluster boundaries. The default is `-100`.
+#' @param max.x Maximum x coordinate of the new cluster boundaries. The default is `100`.
+#' @param min.y Minimum y coordinate of the new cluster boundaries. The default is `-100`.
+#' @param max.y Maximum y coordinate of the new cluster boundaries. The default is `100`.
 #'
-#' @return Nothing. Saves the cleaned cluster as a new cluster with a specified name.
-#'
-#' @import Seurat
-#' @importFrom SeuratObject Idents
-#' @importFrom ggplot2 ggplot
-#' @importFrom magrittr %>%
-#'
-#' @examples
-#' CropCluster(SeuData, ClusterName = "Cluster1", min.x = -50, max.x = 50, min.y = -50, max.y = 50)
-#'
+#' @return Invisibly returns a Seurat UMAP plot of all clusters in the data.
+#'        This function also generates two global variables, one for the cleaned cluster
+#'        and one for the leftover cells, and adds them to the global list "datacleaning_list".
+#' @importFrom Seurat DimPlot
+#' @importFrom SeuratObject Idents WhichCells
 #' @export
-CropCluster = function(SeuData, ClusterName, ClusterNo = 0,
+#'
+
+
+cropCluster = function(SeuData, ClusterName, ClusterNo = 0,
                        min.x = -100, max.x = 100,
                        min.y = -100, max.y = 100){
 
+
+
+  # if "datacleaning_list" does not exist in the global environment, generate it
+  if (!exists("datacleaning_list", envir = globalenv())) {
+    assign("datacleaning_list", list(), envir = globalenv())
+  }
 
   # get cluster names:
   Clusters <- levels(SeuratObject::Idents(SeuData))
 
   # get the UMAP coordinates of each cell
   UMAP_Data <- as.data.frame(SeuData@reductions$umap@cell.embeddings)
-
 
 
 
@@ -66,7 +63,7 @@ CropCluster = function(SeuData, ClusterName, ClusterNo = 0,
   Cluster_Coords <- subset(UMAP_Data, row.names(UMAP_Data) %in% cluster.barcodes)
 
   # look at the selected cluster on UMAP
-  check.cluster <- CheckUMAP1(Cluster_Coords, sample = SeuData)
+  check.cluster <- CheckUMAP(Cluster_Coords, sample = SeuData, color = "blue", label.text = ClusterName)
   print(check.cluster)
 
   ###
@@ -81,15 +78,23 @@ CropCluster = function(SeuData, ClusterName, ClusterNo = 0,
 
 
   print(paste("Cells left in", ClusterName ,"cluster:", dim(Cluster_Clean)[1], sep = " "))
-  print(paste("Current number of cells in current Leftovers must be", length(cluster.barcodes) - dim(Cluster_Clean)[1], sep = " "))
+  print(paste("Number of cells in Current_Leftovers will be:", length(cluster.barcodes) - dim(Cluster_Clean)[1], sep = " "))
 
   # look at the new cluster on UMAP
-  check.cropped.cluster <- CheckUMAP2(Cluster_Clean, sample = SeuData)
+  check.cropped.cluster <- CheckUMAP(Cluster_Clean, sample = SeuData, color = "#008000", label.text = paste0("new_", ClusterName))
   print(check.cropped.cluster)
 
-  # Save the _Clean cluster
-  set.name <- paste0(ClusterName, "_Clean")     # eg."POMC_Clean"
-  assign(set.name, Cluster_Clean, envir = globalenv())
+  # Save the _Clean cluster to the "datacleaning_list"
+  set.clean.name <- paste0(ClusterName, "_Clean")     # eg."POMC_Clean"
+
+  # get the current "datacleaning_list"
+  datacleaning_list <- get("datacleaning_list", envir = globalenv())
+
+  # add the new object to the list
+  datacleaning_list[[set.clean.name]] <- Cluster_Clean
+
+  # update "datacleaning_list" in the global environment
+  assign("datacleaning_list", datacleaning_list, envir = globalenv())
 
 
   ###
@@ -100,10 +105,23 @@ CropCluster = function(SeuData, ClusterName, ClusterNo = 0,
   print(paste("Current number of cells in current Leftovers is", dim(Current_Leftovers)[1], sep = " "))
 
   # look at the Leftovers on UMAP
-  check.leftovers <- CheckUMAP3(Current_Leftovers, sample = SeuData)
+  check.leftovers <- CheckUMAP(Current_Leftovers, sample = SeuData, color = "#cb181d", label.text = "removed_cells")
   print(check.leftovers)
+
+  # Save the _Leftover cluster to the "datacleaning_list"
+  set.leftover.name <- paste0(ClusterName, "_Leftover")     # eg."POMC_Leftover"
+
+  # get the current "datacleaning_list"
+  datacleaning_list <- get("datacleaning_list", envir = globalenv())
+
+  # add the new object to the list
+  datacleaning_list[[set.leftover.name]] <- Current_Leftovers
+
+  # update "datacleaning_list" in the global environment
+  assign("datacleaning_list", datacleaning_list, envir = globalenv())
+
 
   # look at all clusters overall
   print(Seurat::DimPlot(SeuData, reduction = "umap", label  =TRUE))
 
-  }
+}
